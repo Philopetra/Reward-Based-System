06eec96bee069e03ba69dbc906afd0f6d2a4a6fc
xlusivecharles <charlesblaze193@gmail.com>
2024-01-16 23:34:05 +0100

    first commit

--- RYT/Controllers/DashboardController.cs
+++ RYT/Controllers/DashboardController.cs
@@ -8,5 +8,10 @@ namespace RYT.Controllers
         {
             return View();
         }
+
+        public IActionResult TeacherChangePassword()
+        {
+            return View();  
+        }
     }
 }
