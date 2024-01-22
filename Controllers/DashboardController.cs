51bbb039325972b295627aaaaa95c50f1ff8faf3
Abiola Toluwanimi Esther <137096964+NIMI001@users.noreply.github.com>
2024-01-22 15:34:30 +0100

    commit GetList of Scools

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,9 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RYT.Data;
+using RYT.Models.Entities;
+using RYT.Models.ViewModels;
+using RYT.Services.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace RYT.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly IRepository _repository;
+        public DashboardController(IRepository repository)
+        {
+            _repository = repository;
+        }
+
         public IActionResult Overview()
         {
             return View();
@@ -15,11 +29,35 @@ namespace RYT.Controllers
             return View();
         }
 
-        public IActionResult SendReward()
-        { 
-            return View();
+        public IActionResult SendReward(ListOfSchoolViewModel model, string searchString, int page = 1)
+        {
+            const int pageSize = 5;
+
+            IQueryable<string> schools = SeedData.Schools.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                schools = schools.Where(s => s.IndexOf(searchString, StringComparison.OrdinalIgnoreCase) == 0);
+                //schools = schools.Where(s => s.Contains(searchString));
+                
+            }
+
+            int totalItems = schools.Count();
+            int totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
+
+            List<string> schoolsOnPage = schools
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            model.Schools = schoolsOnPage;
+            model.CurrentPage = page;
+            model.TotalPages = totalPages;
+            model.Count = totalItems;
+
+            return View(model);
         }
-        
+
         public IActionResult EditProfile() 
         {
             return View();
--- Data/SeedData.cs
+++ Data/SeedData.cs
@@ -1,4 +1,6 @@
-﻿using RYT.Models.Entities;
+﻿using Org.BouncyCastle.Utilities;
+using RYT.Models.Entities;
+using System.Text.RegularExpressions;
 
 namespace RYT.Data
 {
@@ -62,15 +64,61 @@ namespace RYT.Data
             }
         };
 
-        public static IList<string> Schools { get; set; } = new List<string>
-        {
-            "Mainland Senior High School",
-            "Wesley Girls Junior Secondary School",
-            "Ebute- Metta High School",
-            "Marywood Senior Grammar School",
-            "Akoka Junior High School",
-            "Herbert Macaulay Junior Secondary School"
-        };
+            public static IList<string> Schools { get; set; } = new List<string>
+            {
+                "Mainland Senior High School",
+                "Wesley Girls Junior Secondary School",
+                "Ebute-Metta High School",
+                "Marywood Senior Grammar School",
+                "Akoka Junior High School",
+                "Herbert Macaulay Junior Secondary School",
+                "Queens College",
+                "St. Gregory's College",
+                "Government College, Ikorodu",
+                "Federal Government College, Lagos",
+                "St. Finbarr's College",
+                "Holy Child College",
+                "CMS Grammar School",
+                "Girls' Secondary Grammar School, Ikorodu",
+                "Kings College",
+                "Igbobi College",
+                "Federal Science and Technical College, Yaba",
+                "Ansar-Ud-Deen College, Isolo",
+                "Lagos State Model College, Kankon",
+                "Lagos State Senior Model College, Badore",
+                "Yaba College of Technology Secondary School",
+                "Oriwu Model College, Ikorodu",
+                "Dowen College",
+                "Lagos Anglican Girls Grammar School",
+                "Caleb International College",
+                "St. Louis College",
+                "British International School",
+                "Atlantic Hall",
+                "Ronik Comprehensive School",
+                "Holly Hall Academy",
+                "Halifield Schools",
+                "Chrisland College",
+                "Topfaith International Secondary School",
+                "Greenwood House School",
+                "Doregos Private Academy",
+                "Avi-Cenna International School",
+                "Anwar ul-Islam Girls High School",
+                "Atlantic Hall",
+                "Lagoon Secondary School, Lekki",
+                "Lagos Preparatory School",
+                "Lagos State Junior Model College Badore",
+                "Lagos State Junior Model College Kankon",
+                "Lagos State Model College Badore",
+                "Lagos State Model College, Igbonla",
+                "Lagos State Model College Kankon",
+                "Lagos State Model Junior College Meiran",
+                "Lebanese Community School",
+                "Lekki British School",
+                "Logic Group of Schools",
+                "Lycée Français Louis Pasteur de Lagos"
+
+            };
+
 
         public static IList<string> Subjects { get; set; } = new List<string>
         {
