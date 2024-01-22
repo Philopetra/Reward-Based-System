118b4a0c9ab4a39f6909169210bb03af73809bbf
Neo4ania <neo4ania@gmail.com>
2024-01-22 14:39:58 +0100

    Modified my neo.css file, TeacherListViewModel.cs file and Teachers.cshtml file.

--- Models/ViewModels/TeacherListViewModel.cs
+++ Models/ViewModels/TeacherListViewModel.cs
@@ -9,14 +9,31 @@ namespace RYT.Models.ViewModels
         public string SchoolName { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
         public int PageSize { get; set; } = 10;
-        public int CurrentPage { get; set; } = 1;
+        public int CurrentPage { get; set; } 
         public int TotalPages { get; set; }
         public int Count { get; set; }
+
 
 
         // Additional properties for search and pagination
         public string SearchKeyword { get; set; } = string.Empty;
         public string SearchCriteria { get; set; } = "All";
+
+        public bool PreviousPage
+        {
+            get
+            {
+                return (CurrentPage > 1);
+            }
+        }
+
+        public bool NextPage
+        {
+            get
+            {
+                return ((CurrentPage < TotalPages) || (CurrentPage == TotalPages));
+            }
+        }
 
     }
 }
