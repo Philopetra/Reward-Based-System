9d4e5f520c0389b8b2df8e2e5e08cda484358ce6
Agbajidaniel9@gmail.com <Agbajidaniel9@gmail.com>
2024-01-23 22:54:10 +0100

    First Commit

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RYT.Models.Entities;
+using RYT.Models.ViewModels;
+using RYT.Services.Emailing;
+
+
 
 namespace RYT.Controllers
 {
     public class AccountController : Controller
     {
+
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly IEmailService _emailService;
+
+        public AccountController(UserManager<AppUser> userManager, IEmailService emailService,
+            SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _emailService = emailService;
+            _signInManager = signInManager;
+
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -26,6 +46,42 @@ namespace RYT.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult ResetPassword(string Email, String Token)
+        {
+            var resetPasswordModel = new ResetPasswordViewModel { Email = Email, Token = Token };
+            return View(resetPasswordModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+              var user =   await _userManager.FindByEmailAsync(model.Email);
+                if (user != null)
+                {
+                   var resetPasswordResult = await _userManager.ResetPasswordAsync(user, model.Token, model.NewPassword);
+                    if (resetPasswordResult.Succeeded)
+                    {
+                        return RedirectToAction("Login", "Account");
+                    }
+                    else
+                    {
+                        foreach (var error in resetPasswordResult.Errors)
+                        {
+                            ModelState.AddModelError(error.Code, error.Description);
+                        }
+                        return View(model);
+                    }
+                }
+                ModelState.AddModelError("", "Email Not Recognized");
+            }
+            return View();
+        }
+
         public IActionResult Login()
         {
             return View();
