25ee62eb38723fd24ff45afa6f8eff65748eac02
ngwueche <wisdomngwueche@gmail.com>
2024-01-23 02:27:26 +0100

    added sendrewardVM

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -1,16 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using RYT.Services.Payment;
 
 namespace RYT.Controllers
 {
     public class DashboardController : Controller
     {
-        private readonly IPaymentService _paymentService;
-
-        public DashboardController(IPaymentService paymentService)
-        {
-            _paymentService = paymentService;
-        }
         public IActionResult Overview()
         {
             return View();
---
+++ Models/ViewModels/SendRewardVM.cs
@@ -0,0 +1,17 @@
+﻿using RYT.Models.Enums;
+
+namespace RYT.Models.ViewModels
+{
+    public class SendRewardVM
+    {
+        public string WalletId { get; set; }
+        public decimal Amount { get; set; }
+        public string SenderId { get; set; }
+        public string ReceiverId { get; set; }
+        public string TransactionType { get; set; } = TransactionTypes.Funding.ToString();
+        public string Description { get; set; }
+        public bool Status { get; set; }
+        public string Reference { get; set; }
+
+    }
+}
