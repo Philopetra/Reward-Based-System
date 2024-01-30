55fc5b5ca759b80fa011f691101c91bc570b89b9
Utobi Boniface <utobonnyb@gmail.com>
2024-01-30 14:00:00 +0100

    student signup

--- Models/ViewModels/StudentSignUpViewModel.cs
+++ Models/ViewModels/StudentSignUpViewModel.cs
@@ -9,7 +9,7 @@ namespace RYT.Models.ViewModels
         //[Required]
         ////public string LastName { get; set; } = string.Empty;
         [Required]
-        public string FullName {  get; set; } = string.Empty;
+        public string? FullName {  get; set; } = string.Empty;
 
         public string FirstName { get
             {
